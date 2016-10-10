4fb7cc870f7b75f6988d91b5ca44cda5de2292b9
Aileen OBriengit config --global user.name Aileen <aileentobrien@gmail.com>
2016-10-10 23:50:55 +0000

    Doodled my name in chalk

---
+++ index.js
@@ -0,0 +1,4 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.blue("my ") + chalk.green("name ") + chalk.yellow("is ") + chalk.blue("A") + chalk.green("i") + chalk.yellow("l") + chalk.blue("ee") + chalk.green("n");
+console.log(message);
